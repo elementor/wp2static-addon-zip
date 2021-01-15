0e9ae8310a65f502d0cb6963fad4dee48bb0da78
Leon <me@ljs.dev>
2021-01-15 11:51:24 +1000

    analyse views

--- views/zip-page.php
+++ views/zip-page.php
@@ -1,5 +1,13 @@
+<?php
+// phpcs:disable Generic.Files.LineLength.MaxExceeded                              
+// phpcs:disable Generic.Files.LineLength.TooLong                                  
 
-<p><i><a href="<?php echo admin_url('admin.php?page=wp2static-zip'); ?>">Refresh page</a> to see latest status</i><p>
+/**
+ * @var mixed[] $view
+ */
+?>
+
+<p><i><a href="<?php echo admin_url( 'admin.php?page=wp2static-zip' ); ?>">Refresh page</a> to see latest status</i><p>
 
 <table class="widefat striped">
     <thead>
@@ -14,7 +22,7 @@
             <td>
                 <?php if ( $view['zip_path'] ) : ?>
                     <?php echo $view['zip_size']; ?>
-                <?php else: ?>
+                <?php else : ?>
                     No ZIP found.
                 <?php endif; ?>
             </td>
@@ -30,9 +38,9 @@
 <form
     name="wp2static-zip-delete"
     method="POST"
-    action="<?php echo esc_url( admin_url('admin-post.php') ); ?>">
+    action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>">
 
-    <?php wp_nonce_field( $view['nonce_action'] ); ?>
+                    <?php wp_nonce_field( $view['nonce_action'] ); ?>
     <input name="action" type="hidden" value="wp2static_zip_delete" />
 
     <button class="button btn-danger">Delete ZIP</button>
